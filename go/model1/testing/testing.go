7a199460ce5cf952ee1e93f9eaa57805c2798c97
Drake Wilson <drake@dasyatidae.com>
2015-05-26 14:04:22 -0500

    go/model1: Minor improvements to model test output
    In particular, include -log2 of output shaped distribution.

--- go/model1/testing/testing.go
+++ go/model1/testing/testing.go
@@ -2,7 +2,9 @@ package testing1
 
 import (
 	cryptoRand "crypto/rand"
-
+	"fmt"
+	"math"
+	"strings"
 	"testing"
 	"time"
 )
@@ -59,14 +61,19 @@ func TestExpectedPerformance(t *testing.T, enc Encoder) {
 		t.Fatalf("cannot get random bytes: %v", err)
 	}
 
+	var shapedDist [256]uint64
+
 	totalSource := uniformChunkLen * uniformChunkCount
 	var sourceConsumed int
 	var shapedProduced int
-	garbageOut := make([]byte, uniformChunkLen)
+	shapedOut := make([]byte, uniformChunkLen)
 	for i := 0; i < uniformChunkCount; i++ {
+		// If we can't read any more random bytes, just reuse the existing ones---this is only a
+		// rough statistical test, so that should be okay so long as the chunks are long enough.
+		_, _ = cryptoRand.Read(uniformSource)
 		tail := uniformSource
 		for len(tail) > 0 {
-			dn, sn := enc.ShapeBytes(garbageOut, tail)
+			dn, sn := enc.ShapeBytes(shapedOut, tail)
 			switch {
 			case dn == 0 && sn == 0:
 				t.Fatalf("encoder made no progress in ShapeBytes")
@@ -74,10 +81,14 @@ func TestExpectedPerformance(t *testing.T, enc Encoder) {
 				t.Fatalf("encoder claims to have produced %d bytes", dn)
 			case sn < 0:
 				t.Fatalf("encoder claims to have produced %d bytes", sn)
-			case dn > len(garbageOut):
-				t.Fatalf("encoder claims to have produced %d > %d bytes", dn, len(garbageOut))
+			case dn > len(shapedOut):
+				t.Fatalf("encoder claims to have produced %d > %d bytes", dn, len(shapedOut))
 			case sn > len(tail):
 				t.Fatalf("encoder claims to have consumed %d > %d bytes", sn, len(tail))
+			}
+
+			for _, b := range shapedOut[:dn] {
+				shapedDist[b]++
 			}
 
 			sourceConsumed += sn
@@ -94,6 +105,17 @@ func TestExpectedPerformance(t *testing.T, enc Encoder) {
 	expansionPercent := 100.0 * (expansionRatio - 1.0)
 	t.Logf("simulated average shaped/uniform expansion: %+2.f%% (%d / %d)", expansionPercent, shapedProduced, sourceConsumed)
 	t.Logf("expected uniform transfer rate: %0.2e B/s = %0.2e b/s", bytesPerSecond / expansionRatio, bitsPerSecond / expansionRatio)
+
+	var shapedDistDisplay [256]string
+	for i, n := range shapedDist {
+		negLog := -math.Log2(float64(n) / float64(shapedProduced))
+		if math.IsInf(negLog, 0) {
+			shapedDistDisplay[i] = "*"
+		} else {
+			shapedDistDisplay[i] = fmt.Sprintf("%0.1f", negLog)
+		}
+	}
+	t.Logf("shaped byte distribution -log: %s", strings.Join(shapedDistDisplay[:], " "))
 }
 
 func TestOneDirection(t *testing.T, enc Encoder, dec Decoder) {
